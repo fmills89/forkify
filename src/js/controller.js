3b8a8ee4723ca790956f6bb56d723e6e0001ce43
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-06-10 18:48:52 -0500

    worked on loading by id

--- src/js/controller.js
+++ src/js/controller.js
@@ -30,10 +30,14 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
     // 1) Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -156,4 +160,6 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+// showRecipe();
+
+['hashchange', 'load'].forEach(e => window.addEventListener(e, showRecipe));
