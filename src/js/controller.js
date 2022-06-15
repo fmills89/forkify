e5b874925dcdb628cc3d525c52b7679363f2274e
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-06-14 21:46:18 -0500

    rendered search results - error handling

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,14 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -24,6 +29,7 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
     // Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -31,8 +37,7 @@ const controlSearchResults = async function () {
     // Load search results
     await model.loadSearchResults(query);
     // Render results
-
-    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
