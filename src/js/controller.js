458560bbba2e3a879a45da9828301d840facada9
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-06-16 13:01:40 -0500

    refactored result/bookmark view

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -21,6 +22,7 @@ const controlRecipes = async function () {
 
     // update results view to mark selected search results
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // 1) Loading recipe
     await model.loadRecipe(id);
@@ -65,10 +67,15 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+  // add/remove boookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.deleteBookmark(model.state.recipe.id);
-  console.log(model.state.recipe);
+
+  // update recipe view
   recipeView.update(model.state.recipe);
+
+  // renders bookmarks
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
